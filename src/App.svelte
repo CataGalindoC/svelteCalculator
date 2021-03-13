48302cd12d8cd1385bb62b17f8ab7bd4d1304230
CataGalindoC <catalina.galindoc@gmail.com>
2021-03-12 21:35:13 -0500

    change h1

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@ let operator = '';
 
 </script>
 
-<h1>I'm ready!</h1>
+<h1>holiiiiiiiiiiiiii!</h1>
 
 {#each arrayNumbers as number}
 <button on:click={function(){saveNumber(number)}}>{number}</button>
